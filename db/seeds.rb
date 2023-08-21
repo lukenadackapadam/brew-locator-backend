d8cd6fd3974927acabf1b959f2dbbbbc70a6cdba
“Nickhil <suryanscrit@gmail.com>
2023-08-21 13:46:34 -0500

    added controller,view,routes(index,show)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-require "csv"
+# require "csv"
 
-CSV.foreach(Rails.root.join("lib/assets/breweries_us.csv"), headers: true) do |row|
-  Brewery.create!(
-    brewery_name: row["brewery_name"],
-    type_of_brewery: row["type_of_brewery"],
-    address: row["address"],
-    website: row["website"],
-    state: row["state"],
-    state_breweries: row["state_breweries"],
-  )
-end
+# CSV.foreach(Rails.root.join("lib/assets/breweries_us.csv"), headers: true) do |row|
+#   Brewery.create!(
+#     brewery_name: row["brewery_name"],
+#     type_of_brewery: row["type_of_brewery"],
+#     address: row["address"],
+#     website: row["website"],
+#     state: row["state"],
+#     state_breweries: row["state_breweries"],
+#   )
+# end
